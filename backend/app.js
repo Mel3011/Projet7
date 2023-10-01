7de2ce63672d4a1682b8628ffd82425a2347edcf
Mélanie Truffy <melanie.tr@hotmail.fr>
2023-10-01 23:03:09 +0200

    connect mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://melanietr:a2HWG0dtm7sAN64S@cluster0.d8dfbpz.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
 app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+  res.json({ message: "Votre requête a bien été reçue !" });
 });
 
 module.exports = app;
