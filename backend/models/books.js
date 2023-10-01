b49b0eb98d3105f29c4102ca80a662e7d080ce1b
Mélanie Truffy <melanie.tr@hotmail.fr>
2023-10-02 00:28:18 +0200

    connect to mongoDB

---
+++ backend/models/books.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ratingsSchema = new Schema({
+  userId: { type: String, required: true },
+  grade: { type: Number, required: true },
+});
+
+const bookSchema = new Schema({
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  year: { type: Number, required: true },
+  genre: { type: String, required: true },
+  ratings: [ratingsSchema],
+  averageRating: { type: Number, required: true },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = { Book };
