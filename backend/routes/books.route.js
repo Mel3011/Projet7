97c69e11a3982933f1d1f0ba3d4f7816a9370485
Mélanie TRUFFY <m.truffy@local.fr>
2023-10-03 14:48:28 +0200

    signup et login fonctionnels

--- backend/routes/books.route.js
+++ backend/routes/books.route.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const booksCtrl = require("../controllers/books.controller");
+const auth = require('../middleware/auth');
 
-router.get("/", booksCtrl.findAll);
-router.get("/:id", booksCtrl.findOneById);
-router.get("/bestrating", booksCtrl.findTopRated);
-router.post("/", booksCtrl.addBook);
-router.put("/:id", booksCtrl.updateBook);
-router.delete("/:id", booksCtrl.deleteBook);
-router.post("/:id/rating", booksCtrl.rateBook);
+router.get("/", auth, booksCtrl.findAll);
+router.get("/:id", auth, booksCtrl.findOneById);
+router.get("/bestrating", auth, booksCtrl.findTopRated);
+router.post("/", auth, booksCtrl.addBook);
+router.put("/:id", auth, booksCtrl.updateBook);
+router.delete("/:id", auth, booksCtrl.deleteBook);
+router.post("/:id/rating", auth, booksCtrl.rateBook);
 
 module.exports = router;
